f0167a143d02930c6533dfdf28c893a906155881
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-06-07 22:02:10 +0200

    Beginning of header

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,26 +1,15 @@
 <script setup>
   import { RouterView } from 'vue-router'
-  import { watch } from 'vue'
-  import { ref } from 'vue'
-  import { StaticContentProvider } from './services/StaticContentService';
-
-  const staticInitComplete = ref(StaticContentProvider.INIT_COMPLETE)
-
-  watch(() => StaticContentProvider.INIT_COMPLETE, (newStaticInitComplete) => {
-    staticInitComplete.value = newStaticInitComplete;
-  })
+  import TopHeader from '@/components/TopHeader.vue'
 </script>
 
 <template>
-  <p>Hello</p>
-  <p>{{ staticInitComplete }}</p>
-  <template v-if="staticInitComplete">
-    <!--<RouterView />-->
-  </template>
-  <template v-if="!staticInitComplete">
-    <p>Loading page content</p>
-  </template>
+  <TopHeader />
+  <RouterView />
 </template>
 
 <style scoped>
+  Header {
+    height: 6rem;
+  }
 </style>
---
+++ site/src/components/TopHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <img src="../assets/media/logo-full-black.webp">
+        <nav>
+
+        </nav>
+    </header>
+</template>
+
+<style scroped>
+    header {
+        background-color: var(--color-primary);
+        display: grid;
+        grid-template-columns: 2fr 6fr 1fr;
+        gap: 5rem;
+        padding: 1rem;
+    }
+
+    header img {
+        height: 4rem;
+    }
+</style>
