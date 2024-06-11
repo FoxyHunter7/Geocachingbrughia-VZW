bce4f640ec871f344aee69279ff38d32c7386b5b
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-06-11 17:56:46 +0200

    header with navigation

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,15 +1,63 @@
 <script setup>
+import { onMounted, ref, computed } from 'vue';
   import { RouterView } from 'vue-router'
   import TopHeader from '@/components/TopHeader.vue'
+
+  const innerWidth = ref(window.innerWidth);
+  const isSideMenuOpen = ref(false);
+  const isMobile = computed(() => {
+    return innerWidth.value <= 850;
+  });
+
+  onMounted(() => {
+        window.addEventListener('resize', () => {innerWidth.value = window.innerWidth});
+  });
 </script>
 
 <template>
-  <TopHeader />
+  <section id="side-menu" :class="{ open: isSideMenuOpen }"></section>
+  <TopHeader :isMobile="isMobile" @menu-state-change="(state) => {isSideMenuOpen = state}"/>
   <RouterView />
 </template>
 
 <style scoped>
   Header {
     height: 6rem;
+    box-sizing: border-box;
+    max-width: 100dvw;
+    overflow: hidden;
+  }
+
+  #side-menu {
+    background-color: var(--color-secondary);
+    position: absolute;
+    top: 6rem;
+    right: 0;
+    width: 0;
+    max-width: 80dvw;
+    overflow-x: hidden;
+    height: calc(100lvh - 6rem);
+    transition: width ease 0.3s;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 3rem;
+  }
+  
+  #side-menu.open {
+    width: 15rem;
+    transition: width ease 0.3s;
+  }
+  
+  #side-menu p {
+    height: 1.5rem;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  #side-menu.open p {
+    height: fit-content;
+    overflow: visible;
+    text-overflow: default;
   }
 </style>
--- site/src/components/TopHeader.vue
+++ site/src/components/TopHeader.vue
@@ -1,13 +1,32 @@
 <script setup>
+    import { ref } from 'vue';
+    import { RouterLink } from 'vue-router';
+    import LanguageProvider from '@/services/LanguageService';
+    import { StaticContentProvider as SCP } from '@/services/StaticContentService';
+
+    const props = defineProps(["isMobile"]);
+
+    const lang = ref(LanguageProvider.CURR_LANG);
+    const isNavOpen = ref(false);
+
+    const dictionary = SCP.DICTIONARY;
+    const scsErrors = SCP.ERRORS;
 
 </script>
 
 <template>
     <header>
-        <img src="../assets/media/logo-full-black.webp">
-        <nav>
-
-        </nav>
+        <img v-if="!isMobile" src="../assets/media/logo-full-black.webp" class="logo">
+        <img v-if="isMobile" src="../assets/media/logo-head-black.webp" class="logo">
+        <Teleport to="#side-menu" :disabled="!isMobile">
+            <nav>
+                <RouterLink :to="{ path: SCP.constructRoute(lang, 'NavHome') }">{{ dictionary.NavHome[lang] }}</RouterLink>
+                <RouterLink :to="{ path: SCP.constructRoute(lang, 'NavEvents') }">{{ dictionary.NavEvents[lang] }}</RouterLink>
+                <RouterLink :to="{ path: SCP.constructRoute(lang, 'NavGeocaches') }">{{ dictionary.NavGeocaches[lang] }}</RouterLink>
+                <RouterLink :to="{ path: SCP.constructRoute(lang, 'NavShop') }">{{ dictionary.NavShop[lang] }}</RouterLink>
+            </nav>
+        </Teleport>
+        <div v-if="isMobile" class="nav" :class="{open: isNavOpen}" @click="isNavOpen = !isNavOpen; $emit('menuStateChange', isNavOpen)"></div>
     </header>
 </template>
 
@@ -15,12 +34,62 @@
     header {
         background-color: var(--color-primary);
         display: grid;
-        grid-template-columns: 2fr 6fr 1fr;
+        grid-template-columns: 2fr 6fr;
         gap: 5rem;
         padding: 1rem;
     }
 
-    header img {
+    header > img.logo {
         height: 4rem;
+    }
+
+    header > nav {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        gap: 1.8rem;
+    }
+
+    #side-menu > nav {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 1.5rem;
+        margin: 2rem 2.5rem;
+    }
+
+    header > .nav {
+        margin: auto 0 auto auto;
+        height: 80%;
+        aspect-ratio: 1 / 1;
+        background-color: var(--color-text);
+        mask: url(../assets/media/menu.svg);
+        mask-size: contain;
+    }
+
+    header > .nav.open {
+        mask: url(../assets/media/x.svg);
+        mask-size: contain;
+    }
+
+    nav a {
+        text-decoration: none;
+        text-transform: capitalize;
+        font-size: 1rem;
+        font-weight: normal;
+        color: var(--color-text);
+        border-bottom: 0.2rem solid rgba(0, 0, 0, 0);
+        transition: border-bottom, font-weight 0.1s;
+    }
+
+    nav a:hover {
+        border-bottom: 0.2rem solid var(--color-secondary);
+        transition: border-bottom, font-weight 0.1s;
+    }
+
+    nav a.router-link-active {
+        border-bottom: 0.2rem solid var(--color-text);
+        transition: border-bottom, font-weight 0.1s;
+        font-weight: bold;
     }
 </style>
