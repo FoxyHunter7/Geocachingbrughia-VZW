947f1f64383279350b736aebfd7f71400d190c3d
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-06-27 01:36:31 +0200

    home hero loader

--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -9,6 +9,7 @@
 
   const events = ref(["loading"]);
   const messages = ref([]);
+  const loaderActive = ref(false);
   const lang = computed(() => LanguageProvider.CURR_LANG.value);
 
   async function fetchData() {
@@ -16,9 +17,15 @@
     messages.value = await getAllMessages();
   }
 
+  async function initLoaderOnDelay() {
+    await new Promise(r => setTimeout(r, 2000));
+    loaderActive.value = true;
+  }
+
   onMounted(fetchData);
   watch(lang, async () => await fetchData());
 
+  initLoaderOnDelay();
 </script>
 
 <template>
@@ -26,7 +33,10 @@
     <Message v-for="message in messages" :message="message" />
   </Teleport>
   <main>
-    <section v-show=" events.length !== 0 && events[0] === 'loading'" id=loading><p>Loading</p></section>
+    <section v-show=" events.length !== 0 && events[0] === 'loading'" id=loading>
+      <div v-show="loaderActive" class="initial-loader">
+      </div>
+    </section>
     <Hero v-show="events.length === 0" id="default-hero"/>
     <Event v-if="events.length !== 0 && events[0] !== 'loading'" v-for="event in events" :event="event" />
   </main>
@@ -42,5 +52,11 @@
   section {
     height: 100%;
     max-height: 50rem;
+  }
+
+  #loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
