cb39d9488271f2eb404083ae84d70856884b2f30
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-06-27 14:02:37 +0200

    Socials Section Homepage

---
+++ site/src/components/Socials.vue
@@ -0,0 +1,78 @@
+<script setup>
+    import { computed } from 'vue';
+    import LanguageProvider from '@/services/LanguageService.js';
+    import StaticContentProvider from '@/services/StaticContentService.js';
+    import config from '../data/config.json';
+
+    const props = defineProps({
+        socials: Array
+    });
+
+    const lang = computed(() => LanguageProvider.CURR_LANG.value);
+</script>
+
+<template>
+    <section>
+        <h2>{{ StaticContentProvider.DICTIONARY.SocialsFollowTxt[lang] }}</h2>
+        <div>
+            <figure v-for="social in socials">
+                <img :src="`${config.apiUrl}images/${social.imageUrl}`">
+                <p><a :href="social.url" target="_blank">{{ social.name }}</a></p>
+            </figure>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    section {
+        height: auto !important;
+        padding: 3rem;
+        background-color: var(--color-background2)
+    }
+
+    section div {
+        display: flex;
+        gap: 3rem;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    }
+
+    h2 {
+        text-align: center;
+        text-transform: uppercase;
+        margin-bottom: 3rem;
+        font-weight: bold;
+    }
+
+    figure {
+        padding: 1.5rem;
+        border-radius: 0.5rem;
+        height: 10rem;
+        background-color: var(--color-tertiary);
+        transform: scale(100%);
+        transition: transform 0.3s;
+    }
+
+    figure:hover {
+        cursor: pointer;
+        transform: scale(105%);
+        transition: transform 0.3s;
+        text-decoration: underline;
+        color: var(--color-text2);
+    }
+
+    figure img {
+        max-height: 70%;
+        margin-bottom: 0.5rem;
+    }
+
+    figure p {
+        text-align: center
+    }
+
+    figure a {
+        text-decoration: none;
+        text-transform: capitalize;
+        color: var(--color-text2);
+    }
+</style>
--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -2,23 +2,27 @@
   import { computed, onMounted, ref, watch } from 'vue';
   import { getHomePageEvents } from '@/services/EventService';
   import { getAllMessages } from '@/services/MessageService';
+  import { getAllSocials } from '@/services/SocialService';
   import LanguageProvider from '@/services/LanguageService';
   import Message from '@/components/Message.vue';
   import Event from '@/components/Event.vue';
   import Hero from '@/components/Hero.vue';
+  import Socials from '@/components/Socials.vue';
 
   const events = ref(["loading"]);
   const messages = ref([]);
+  const socials = ref([]);
   const loaderActive = ref(false);
   const lang = computed(() => LanguageProvider.CURR_LANG.value);
-
+  
   async function fetchData() {
-    events.value = await getHomePageEvents();
+    socials.value = await getAllSocials();
     messages.value = await getAllMessages();
+    events.value = await getHomePageEvents();
   }
 
   async function initLoaderOnDelay() {
-    await new Promise(r => setTimeout(r, 2000));
+    await new Promise(r => setTimeout(r, 2500));
     loaderActive.value = true;
   }
 
@@ -33,11 +37,12 @@
     <Message v-for="message in messages" :message="message" />
   </Teleport>
   <main>
-    <section v-show=" events.length !== 0 && events[0] === 'loading'" id=loading>
+    <section v-show="events.length !== 0 && events[0] === 'loading'" id=loading>
       <div v-show="loaderActive" class="initial-loader"></div>
     </section>
     <Hero v-show="events.length === 0" id="default-hero"/>
     <Event v-if="events.length !== 0 && events[0] !== 'loading'" v-for="event in events" :event="event" />
+    <Socials v-if="socials.length !== 0 && events[0] !== 'loading'" :socials="socials"/>
   </main>
 </template>
 
