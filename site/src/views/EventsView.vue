ea32dd351de5934e9d586dae37348428e6518c83
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-06-27 16:16:56 +0200

    rudementary events page

--- site/src/views/EventsView.vue
+++ site/src/views/EventsView.vue
@@ -1,11 +1,60 @@
 <script setup>
+  import { getAllEvents } from '@/services/EventService';
+  import LanguageProvider from '@/services/LanguageService';
+  import { computed, onMounted, ref, watch } from 'vue';
+  import Event from '@/components/Event.vue';
+
+  const events = ref(["loading"]);
+  const loaderActive = ref(false);
+  const lang = computed(() => LanguageProvider.CURR_LANG.value);
+
+  const search = ref("");
+
+  async function fetchData() {
+    const pagedResponse = await getAllEvents(search.value);
+    if (pagedResponse.data) {
+      events.value = pagedResponse.data;
+    } else {
+      events.value = pagedResponse;
+    }
+  }
+
+  async function initLoaderOnDelay() {
+    await new Promise(r => setTimeout(r, 2500));
+    loaderActive.value = true;
+  }
+
+  onMounted(fetchData);
+  watch(lang, async () => await fetchData());
+
+  initLoaderOnDelay();
 </script>
 
 <template>
   <main>
-    <p>Events</p>
+    <section v-show="events.length !== 0 && events[0] === 'loading'" id=loading>
+      <div v-show="loaderActive" class="initial-loader"></div>
+    </section>
+    <section v-show="events.length === 0"><p>No events Found</p></section>
+    <Event v-if="events.length !== 0 && events[0] !== 'loading'" v-for="event in events" :event="event" />
   </main>
 </template>
 
 <style scoped>
+  main {
+    flex: 1 1 auto;
+    height: 100%;
+    overflow-y: auto;
+  }
+
+  section {
+    height: 100%;
+    max-height: 50rem;
+  }
+
+  #loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
