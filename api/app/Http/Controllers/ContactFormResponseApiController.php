e8602665e9d58eb74892b1be5dfe546b7dd9d8ec
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-06-27 16:39:32 +0200

    It was the s

--- api/app/Http/Controllers/ContactFormResponseApiController.php
+++ api/app/Http/Controllers/ContactFormResponseApiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Service\ContactFormResponseService;
+use App\Services\ContactFormResponseService;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
