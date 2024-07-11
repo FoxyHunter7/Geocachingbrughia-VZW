35b2c18f038d059d147895dbbcb32dba3af51aaf
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-07-11 12:14:50 +0200

    image no longer required (for update)

--- api/app/Services/EventService.php
+++ api/app/Services/EventService.php
@@ -9,14 +9,14 @@ use Illuminate\Support\MessageBag;
 class EventService extends Service
 {
     protected $_rules = [
-        'state' => 'required | in:ONLINE,DRAFT, ARCHIVED',
+        'state' => 'required | in:ONLINE,DRAFT,ARCHIVED',
         'title' => 'required | max:100 | string',
         'geolink' => 'required | url:https | starts_with:https://www.geocaching.com/geocache/| string',
         'type' => 'required | in:REGULAR,CITO,MEGA,GIGA,BLOCK',
         'location' => 'nullable | regex:/^[NS]\s\d+°\s\d+\.\d+\s[EW]\s\d+°\s\d+\.\d+$/',
         'start_date' => 'required|date_format:Y-m-d H:i:s',
         'end_date' => 'required|date_format:Y-m-d H:i:s',
-        'image' => 'required | image | mimes:jpeg,png,jpg,gif,svg|max:4096'
+        'image' => 'image | mimes:jpeg,png,jpg,gif,svg|max:4096'
     ];
 
     protected $_fields = ['id', "on_home", 'title', 'geolink', 'type', 'location', 'start_date', 'end_date', 'imageUrl'];
