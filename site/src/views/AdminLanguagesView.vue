8464d637ab738e3fe729ff8e15bb6066dec4190d
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-07-11 17:29:14 +0200

    under construction notices

--- site/src/views/AdminLanguagesView.vue
+++ site/src/views/AdminLanguagesView.vue
@@ -2,8 +2,18 @@
 </script>
 
 <template>
-
+    <div>
+        <h1>Nog niet beschikbaar</h1>
+        <p>Wordt verwacht begin Augustus</p>
+    </div>
 </template>
 
 <style scoped>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%
+    }
 </style>
