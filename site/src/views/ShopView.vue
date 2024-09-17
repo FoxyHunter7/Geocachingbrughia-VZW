b9fadac9e23ef7911a2f8d43d0b304a9213b8237
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-09-17 09:32:14 +0200

    functional Shopview structure (actual shopitems missing)

--- site/src/views/ShopView.vue
+++ site/src/views/ShopView.vue
@@ -45,9 +45,119 @@
 
 <template>
   <main>
-    <p>{{ products }}</p>
+    <section v-show="products.length !== 0 && products[0] === 'loading'" id="loading">
+      <div v-show="loaderActive" class="initial-loader"></div>
+    </section>
+    <section id="no-products" v-show="products.length === 0">
+      <h2>{{ dictionary.UINoStoreItems[lang] }}</h2>
+      <p>{{ dictionary.UINoStoreItemsSubTxt[lang] }}</p>
+    </section>
+    <form v-show="products.length > 0 && products[0] !== 'loading'" method="post" @submit.prevent="">
+      <div>
+        <input v-model="search" type="search" id="search" name="search" autocomplete="search" required :placeholder="dictionary.FormSearch[lang]">
+      </div>
+    </form>
   </main>
 </template>
 
 <style scoped>
+  main {
+    position: relative;
+    flex: 1 1 auto;
+    height: 100%;
+    overflow-y: auto;
+    padding-bottom: 1rem;
+  }
+
+  section {
+    height: 70vh;
+    max-height: 40rem;
+    max-width: 100rem;
+    margin: 0 auto;
+  }
+
+  #loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  form {
+    display: flex;
+    justify-content: flex-end;
+    padding: 1rem;
+    gap: 1rem;
+  }
+
+  form > div input {
+    width: 20rem;
+    max-width: 95vw;
+  }
+
+  form > div {
+    position: relative;
+  }
+
+  form > div::before {
+    content: '';
+    height: 1.3rem;
+    width: 1.3rem;
+    position: absolute;
+    left: 0.5rem;
+    top: 0.3rem;
+    background-color: var(--color-text);
+    mask: url(../assets/media/search.svg);
+    mask-size: contain;
+    mask-repeat: no-repeat;
+    mask-position: center;
+  }
+
+  form input, form textarea {
+    height: 2rem;
+    border-radius: 0.3rem;
+    border: solid 0.1rem var(--color-text);
+    outline: none;
+    font-family: inherit;
+    font-size: 1rem;
+    box-sizing: border-box;
+    padding: 0 0.5rem 0 2rem;
+    line-height: 2rem;
+    text-transform: capitalize;
+    background-color: var(--color-background);
+    color: var(--color-text)
+  }
+
+  #no-products {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    padding: 0 1rem;
+  }
+
+  #no-products h2 {
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+
+  #no-products p {
+    text-align: center;
+  }
+
+  #no-products a {
+    color: var(--color-quaternary);
+    text-decoration: none;
+  }
+
+  #no-products a:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 1000px) {
+    form {
+      justify-content: center;
+    }
+  }
 </style>
