c90dc718bab9704d15cdb518f4cb6c424d6566b4
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-09-17 17:10:03 +0200

    beginning of detail view

--- site/src/views/ShopView.vue
+++ site/src/views/ShopView.vue
@@ -8,6 +8,7 @@
   const dictionary = StaticContentProvider.DICTIONARY;
 
   const products = ref(["loading"]);
+  const detailsOf = ref(-1);
 
   async function fetchData() {
     const response = await getAllProducts();
@@ -52,23 +53,74 @@
       <h2>{{ dictionary.UINoStoreItems[lang] }}</h2>
       <p>{{ dictionary.UINoStoreItemsSubTxt[lang] }}</p>
     </section>
-    <form v-show="products.length > 0 && products[0] !== 'loading'" method="post" @submit.prevent="">
+    <form v-show="products.length > 0 && products[0] !== 'loading' && detailsOf === -1" method="post" @submit.prevent="">
       <div>
         <input v-model="search" type="search" id="search" name="search" autocomplete="search" required :placeholder="dictionary.FormSearch[lang]">
       </div>
     </form>
-    <section id="store-items">
-      <article v-if="products.length > 0 && products[0] !== 'loading'" v-for="product in filteredProducts">
+    <section id="store-items" v-show="detailsOf === -1">
+      <article v-if="products.length > 0 && products[0] !== 'loading'" v-for="(product, index) in filteredProducts" @click="detailsOf = index">
         <div>
           <img :src="product.images[0]" data-cookiescript="denied" data-category="functionality">
         </div>
         <p>{{ product.name }} <span>{{ product.price.amount / 100 }} {{ product.price.currency.toUpperCase() }}</span></p>
       </article>
     </section>
+    <section v-if="detailsOf !== -1" id="product-details">
+      <div>
+        <img :src="filteredProducts[detailsOf].images[0]">
+      </div>
+      <div>
+        <h2>{{ filteredProducts[detailsOf].name }} <span>{{ filteredProducts[detailsOf].price.amount / 100 }} {{ filteredProducts[detailsOf].price.currency.toUpperCase() }}</span></h2>
+        <p>{{ filteredProducts[detailsOf].description }}</p>
+      </div>
+    </section>
   </main>
 </template>
 
 <style scoped>
+  #product-details {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    align-items: center;
+    width: 70%;
+    max-width: 80rem;
+    gap: 5rem;
+    margin: auto;
+    height: 100%;
+  }
+
+  #product-details h2 {
+    font-weight: bold;
+  }
+
+  #product-details h2 span {
+    background-color: var(--color-accent-light);
+    padding: 0.1rem 0.5rem;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    margin-left: 1rem;
+    transform: translateY(-0.1rem);
+    display: inline-block;
+  }
+
+  #product-details div {
+    height: 30rem;
+  }
+
+  #product-details div:first-child {
+    display: flex;
+    justify-content: end;
+  }
+
+  #product-details div img {
+    max-height: 80%;
+    max-width: 100%;
+    object-fit: contain;
+    object-position: top;
+    border-radius: 0.5rem;
+  }
+
   #loading {
     height: 80vh;
     max-height: 40rem;
