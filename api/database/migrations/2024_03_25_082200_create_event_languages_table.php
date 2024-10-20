5b22a001b41e06aa36917d8a6189369ab9bb14f4
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-10-20 16:48:44 +0200

    whoops

--- api/database/migrations/2024_03_25_082200_create_event_languages_table.php
+++ api/database/migrations/2024_03_25_082200_create_event_languages_table.php
@@ -1,4 +1,7 @@
-use Illuminate\Database\Migrations\Migration;                                                                                                                             use Illuminate\Database\Schema\Blueprint;
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
