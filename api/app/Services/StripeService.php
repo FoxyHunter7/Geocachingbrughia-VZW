415ce3808ac19d9d2278dabfe69eb6aa63bd1f5f
Matthias Vancoillie (Desktop) <matthias.vancoillie2@student.howest.be>
2024-11-30 13:28:28 +0100

    Revert "remove stripe intigration from backend"
    This reverts commit a870d83ebb7efcbc8217315e3468c1f9ce515844.

---
+++ api/app/Services/StripeService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+class StripeService
+{
+    protected $_stripe;
+
+    public function __construct()
+    {
+
+        $this->_stripe = new \Stripe\StripeClient(env("STRIPE_SK", ""));
+    }
+
+    public function allProducts($admin, $language) {
+        $products = $this->_stripe->products->all($admin ? [] : ['active' => true]);
+
+        $lang_filtered_products = collect($products->data)
+        ->filter(function ($product) use ($language) {
+            return isset($product->metadata['lang']) && ($product->metadata['lang'] === $language || $product->metadata['lang'] === '*');
+        });
+
+        return ['data' => $lang_filtered_products->map(function ($product) {
+            $price = $this->_stripe->prices->retrieve($product->default_price);
+
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'description' => $product->description,
+                'images' => $product->images,
+                'price' => [
+                    'currency' => $price->currency,
+                    'amount' => $price->unit_amount
+                ],
+                'metadata' => $product->metadata
+            ];
+        })->values()];
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -38,6 +38,7 @@ Route::group([
     Route::get('languages', [LanguageApiController::class, 'allPublic']);
     Route::get('static',[StaticSiteContentController::class, 'allPublic']);
     Route::get('socials', [SocialApiController::class, 'allPublic']);
+    Route::get('products', [StripeApiController::class, 'allProductsPublic']);
 
     Route::get('images/{p1}/{filename}', [ImageController::class, 'get'])->name('images');
 
@@ -107,6 +108,10 @@ Route::group([
 
         Route::controller(ContactFormResponseApiController::class)->group(function () {
             Route::get('formresponses', 'all');
+        });
+
+        Route::controller(StripeApiController::class)->group(function() {
+            Route::get('products', 'allProductsAdmin');
         });
     });
 });
